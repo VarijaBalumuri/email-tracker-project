3ecce836650e4afa8ea80f26e5710cf38037f0a5
varija <varijabalumuri8@gmail.com>
2025-05-28 13:48:15 -0400

    Your meaningful commit message

--- src/main/java/com/varija/email_tracker/controller/EmailController.java
+++ src/main/java/com/varija/email_tracker/controller/EmailController.java
@@ -3,6 +3,7 @@ package com.varija.email_tracker.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.varija.email_tracker.entity.Email;
 import com.varija.email_tracker.service.EmailService;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/emails")
 public class EmailController {
